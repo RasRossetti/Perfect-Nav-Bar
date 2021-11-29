2e5e93f65d8169b9b2787b7df1059c7f1eb311ac
Ras Rossetti <rasrossetti@gmail.com>
2021-11-28 19:47:16 -0600

    CSS Changes

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 const hamburger = document.querySelector(".hamburger");
 const backdrop = document.querySelector(".backdrop2");
 const sidebar = document.querySelector(".sidebar2");
+const times = document.querySelector(".times");
 
 hamburger.addEventListener("click", () => {
   toggleSlidebar();
 });
 backdrop.addEventListener("click", () => {
+  toggleSlidebar();
+});
+times.addEventListener("click", () => {
   toggleSlidebar();
 });
 
